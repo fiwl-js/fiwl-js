6fee0137d51391637a6b07f7cdc9bc669c67c994
antriksh123 <antariksha.verma@pathways.in>
2020-11-28 15:19:39 +0530

    Updated checkbox by using render api and draw api

--- src/widgets/Checkbox.ts
+++ src/widgets/Checkbox.ts
@@ -1,33 +1,56 @@
-import Label from "./Label";
 import { EventTypes } from "../event/EventTypesEnum";
 import MouseEventData from "../event/data/MouseEventData";
+import RenderAPI from "../display/renderer/RenderAPI";
+import DisplayObject from "./DisplayObject";
+import InstructTemplate from "../display/renderer/templates/InstructTemplate";
+import StrokePathInstruct from "../display/renderer/instructs/StrokePathInstruct";
+import VertexTemplate from "../display/renderer/templates/VertexTemplate";
 
-export default class Checkbox extends Label {
+export default class Checkbox extends DisplayObject {
   public checked = false;
+  public tickSize = "5dp";
 
   constructor() {
     super();
     this.height = "35px";
     this.width = "35px";
     this.backgroundColor = app.res.style.onSecondaryColor;
-    this.borderColor = app.res.style.textColor;
-    this.borderSize = '2px';
-    this.textSize = "35dp";
-    this.alignText = "center";
+  }
+
+  draw(api: RenderAPI): InstructTemplate[] {
+    let instructs = this.drawBackground(api);
+
+    let checkBox = api.lineRect(
+      this.globalX,
+      this.globalY,
+      this.measuredHeight,
+      this.measuredWidth,
+      app.colors.black,
+      2
+    );
+    instructs.push(checkBox);
+
+    let vertices: VertexTemplate[] = [
+      {x: this.globalX, y: this.globalY + (this.measuredHeight/2)},
+      {x: this.globalX + (this.measuredWidth/4), y: this.globalY + this.measuredHeight},
+      {x: this.globalX + this.measuredWidth, y: this.globalY}
+    ]
+    if (this.checked) {
+      let check = api.linePath(
+        app.colors.blue,
+        vertices,
+        app.unit.resolve(this.tickSize)
+      )
+      instructs.push(check);
+    }
+
+    return this.drawPostEffect(api, instructs);
   }
 
   ready(): void {
     // On click change the checked mark
-    this.addEventListener(
-      EventTypes.SELECT,
-      (eventData: MouseEventData) => {
-        this.checked = !this.checked;
-        if (this.checked) this.text = '✔️'; // ! Change this to use Stroke Path API instead of text
-        else {
-          this.text = '';
-          this.backgroundColor = app.res.style.onSecondaryColor;
-        }
-      }
-    );
+    this.addEventListener(EventTypes.SELECT, (eventData: MouseEventData) => {
+      this.checked = !this.checked;
+    });
   }
 }
