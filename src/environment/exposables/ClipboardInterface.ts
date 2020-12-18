d8fb2b2ecdd4fa243e08bd6067fb122010556dfe
antriksh123 <antariksha.verma@pathways.in>
2020-12-18 21:23:36 +0530

    refactor: convert clipboard api function to interface

---
+++ src/environment/exposables/ClipboardInterface.ts
@@ -0,0 +1,35 @@
+import ParamsTemplate from "../ParamsTemplate";
+
+export default interface ClipboardInterface {
+    /**
+     *  Reads the current clipboard text and returns the text
+     *
+     *  @param {void} Nothing
+     *  @returns {Promise<String>} Text
+     */
+    read: () => Promise<String>;
+
+    /**
+     *  Add a new item to local storage
+     *
+     *  @param {string} Data The data to write
+     *  @returns {Promise<void>} Nothing
+     */
+    write: (data: string) => Promise<void>;
+}
+
+export const name = "storage";
+
+export const bind = async (
+    params: ParamsTemplate
+): Promise<ClipboardInterface> => {
+    const clipboard: ClipboardInterface = {
+        read: () => {
+            return navigator.clipboard.readText();
+        },
+        write: (data: string) => {
+            return navigator.clipboard.writeText(data)
+        }
+    }
+    return Object.freeze(clipboard)
+}
